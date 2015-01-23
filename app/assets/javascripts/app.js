7fda9ceb8e4494b5e9b765853f0bdc77dca718fc
TomBrightbill <tombrightbill@gmail.com>
2015-01-22 16:28:24 -0800

    Pseudo soundManager

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,7 +1,33 @@
 $(document).ready(function(){
 
   $('.add-song').on('click',function(e){
-    console.log($(this).closest('div[class="track"]'));
+    var track_parent = $(this).closest('div[class="track"]');
+    var track_title = track_parent.find('h3[class="title"]').text();
+    var art_url = track_parent.find('span[class="artwork"]').text()
+    var track_url = track_parent.find('span[class="track_url]').text();
+
+  });
+
+  $('.track').on('click', function(){
+    var track_url = $(this).find('span[class="track_url"]').text();
+    console.log(track_url);
+
+    soundManager.onready(function() {
+    // SM2 is ready to go!
+    // soundManager.createSound(), etc.
+    var mySound = soundManager.createSound({
+      id: 'mySound',
+      url: 'https://api.soundcloud.com/tracks/94513154/stream?client_id=1931311a85d3f849f5e5cf60070cffad',
+      autoLoad: true,
+      autoPlay: false,
+      onload: function() {
+      alert('The sound '+this.id+' loaded!');
+        },
+      volume: 50
+      });
+    mySound.play();
+    });
+
   });
 
 });
